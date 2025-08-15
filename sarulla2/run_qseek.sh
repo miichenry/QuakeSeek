0e3389f708049f7b1d658b728369aa3bb6b55e88
Henry Michail <henrymi@login1.yggdrasil>
2025-08-15 12:56:38 +0200

    new

--- sarulla2/run_qseek.sh
+++ sarulla2/run_qseek.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=qseek # create a name for your job
 #SBATCH --partition=shared-gpu,public-gpu ##,public-bigmem,shared-bigmem
 #SBATCH --ntasks=1               # total number of tasks
-#SBATCH --cpus-per-task=5
-#SBATCH --gpus-per-task=2      # cpu-cores per task
+#SBATCH --cpus-per-task=10
+#SBATCH --gpus-per-task=5      # cpu-cores per task
 ##SBATCH --mem=35G         # memory per cpu-core
 #SBATCH --time=12:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --output="outslurm/%x-%j.out"
